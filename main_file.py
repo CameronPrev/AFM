9567e1b891c0b28c3e729aab6ef3fd21b946c19f
Cameron Prevost <47579063+CameronPrev@users.noreply.github.com>
2019-08-07 13:55:20 +0000

    Delete main_file.py

--- main_file.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-#Create database if not exist and get a connection to it
-connection = sqlite3.connect('AFMDB/AFM.db')
-
-# Get a cursor ro execute sql statements
-cursor = connection.cursor()
-
-
